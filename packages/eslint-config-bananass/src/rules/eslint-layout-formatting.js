23046539f28693457e4aefba8f4e66ea47df3439
루밀LuMir <rpfos@naver.com>
2024-12-02 20:46:05 +0900

    wip: create `eslint-layout-formatting.js`

---
+++ packages/eslint-config-bananass/src/rules/eslint-layout-formatting.js
@@ -0,0 +1,18 @@
+/**
+ * This file follows:
+ *
+ * - The order of the rules listed in the 'Layout & Formatting' section on eslint.org.
+ *   - See, {@link https://eslint.org/docs/latest/rules#layout--formatting}.
+ *
+ * - The best practices outlined in `eslint-config-airbnb-base@19.0.4`.
+ *   - See, {@link https://github.com/airbnb/javascript/tree/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules}.
+ */
+module.exports = {
+  /**
+   * Require or disallow Unicode byte order mark (BOM).
+   *
+   * @link eslint: {@link https://eslint.org/docs/latest/rules/unicode-bom}
+   * @link airbnb-base: {@link https://github.com/airbnb/javascript/blob/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules/style.js#L521}
+   */
+  'unicode-bom': ['error', 'never'],
+};
--- packages/eslint-config-bananass/src/rules/index.js
+++ packages/eslint-config-bananass/src/rules/index.js
@@ -1,5 +1,7 @@
+const eslintLayoutFormatting = require('./eslint-layout-formatting');
 const eslintPossibleProblems = require('./eslint-possible-problems');
 
 module.exports = {
+  ...eslintLayoutFormatting,
   ...eslintPossibleProblems,
 };
