96a53b51358f417b4325d4461ceb1ad4bc1cb2d1
루밀LuMir <rpfos@naver.com>
2024-12-07 20:24:45 +0900

    wip: update `index.js`

--- packages/eslint-config-airbnb-base-flat/src/index.js
+++ packages/eslint-config-airbnb-base-flat/src/index.js
@@ -1,8 +1,14 @@
-const rules = require('./rules');
+const eslintLayoutFormatting = require('./rules/eslint-layout-formatting');
+const eslintPossibleProblems = require('./rules/eslint-possible-problems');
+const eslintSuggestions = require('./rules/eslint-suggestions');
 
 module.exports = [
   {
     // languageOptions: {},
-    rules,
+    rules: {
+      ...eslintLayoutFormatting,
+      ...eslintPossibleProblems,
+      ...eslintSuggestions,
+    },
   },
 ];
--- packages/eslint-config-airbnb-base-flat/src/rules/index.js
+++
@@ -1,7 +0,0 @@
-const eslintLayoutFormatting = require('./eslint-layout-formatting');
-const eslintPossibleProblems = require('./eslint-possible-problems');
-
-module.exports = {
-  ...eslintLayoutFormatting,
-  ...eslintPossibleProblems,
-};
