3ab8cc077a66fdff3b837b97a4666e30f57c6191
루밀LuMir <rpfos@naver.com>
2024-12-08 19:48:05 +0900

    chore(examples): create `examples/readline` (#12)
    * chore(example/readline): create `examples/readline`
    
    * wip: create `package.json`
    
    * chore: delete `.gitkeep`
    
    * wip: update `package.json`
    
    * wip

---
+++ examples/readline/index.js
@@ -0,0 +1,24 @@
+/**
+ * This example shows how to use `readline` module.
+ *
+ * For more information, see {@link https://nodejs.org/api/readline.html}.
+ *
+ * - Event `'close'`: {@link https://nodejs.org/api/readline.html#event-close}
+ * - Event `'line'`: {@link https://nodejs.org/api/readline.html#event-line}
+ * - `readline.createInterface(options)`: {@link https://nodejs.org/api/readline.html#readlinecreateinterfaceoptions}
+ */
+
+const { createInterface } = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = createInterface({ input, output });
+
+console.log("Type 'close' to quit");
+
+rl.on('line', line => {
+  console.log(`Received: ${line}`);
+
+  if (line === 'close') rl.close();
+}).on('close', () => {
+  console.log('Closed!');
+});
