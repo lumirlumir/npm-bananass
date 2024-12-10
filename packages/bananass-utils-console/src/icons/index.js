27fe402e636eb5c66173f5811203b1e0b50ad0e8
루밀LuMir <rpfos@naver.com>
2024-12-11 00:03:05 +0900

    wip: create `icons/index.js`

---
+++ packages/bananass-utils-console/src/icons/index.js
@@ -0,0 +1,16 @@
+const c = require('ansi-colors');
+
+const { isUnicodeSupported } = require('../utils');
+
+module.exports.successSymbol = c.green(isUnicodeSupported ? '✔' : '√');
+module.exports.errorSymbol = c.red(isUnicodeSupported ? '✖️' : '×');
+module.exports.warningSymbol = c.yellow(isUnicodeSupported ? '⚠' : '‼');
+module.exports.infoSymbol = c.blue(isUnicodeSupported ? 'ℹ' : 'i');
+module.exports.bananassSymbol = c.yellow(isUnicodeSupported ? '🍌' : '');
+
+module.exports.defaultSpinner = {
+  frames: isUnicodeSupported
+    ? ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
+    : ['-', '\\', '|', '/'],
+  interval: 80,
+};
