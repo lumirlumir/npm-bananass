013fa4ad3b3bf355441b3d017eb026e307bbeb81
루밀LuMir <rpfos@naver.com>
2024-12-17 22:55:36 +0900

    build: update Babel configuration and add TypeScript support (#33)
    This pull request includes several changes focused on updating
    configuration files, improving TypeScript support, and refining the
    build and export processes for the `bananass-utils-console` package.
    Here are the most important changes:
    
    ### Configuration Updates:
    
    * Updated `.github/sync-client.yml` to remove the `babel.config.js`
    synchronization entry.
    
    ### TypeScript Support:
    
    * Added TypeScript as a dependency in `package.json`.
    * Updated the `exports` field in
    `packages/bananass-utils-console/package.json` to include TypeScript
    declaration files.
    * Added a `tsconfig.json` file to `packages/bananass-utils-console` to
    configure TypeScript compilation.
    
    ### Build and Export Process:
    
    * Modified `babel.config.js` to prevent module transformation by Babel.
    * Added a `babel.config.cjs` file to `packages/bananass-utils-console`
    to extend the root `babel.config.js`.
    * Updated the `scripts` section in
    `packages/bananass-utils-console/package.json` to include a build script
    and a `prepublishOnly` script for building before publishing.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+  ],
   ignore: ['**/*.test.js', '**/*.spec.js'],
   minified: true,
   comments: false,
