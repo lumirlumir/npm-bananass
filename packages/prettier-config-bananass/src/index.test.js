5a291a9a773e06a9ade69f9921325c5158410e86
루밀LuMir <rpfos@naver.com>
2025-01-08 18:24:37 +0900

    wip: `index.test.js`

--- packages/prettier-config-bananass/src/index.test.js
+++ packages/prettier-config-bananass/src/index.test.js
@@ -7,7 +7,7 @@
 // --------------------------------------------------------------------------------
 
 const { strictEqual } = require('node:assert');
-const { test } = require('node:test');
+const { describe, it } = require('node:test');
 
 const prettierConfig = require('./index');
 
@@ -15,62 +15,64 @@ const prettierConfig = require('./index');
 // Test
 // --------------------------------------------------------------------------------
 
-test('printWidth should be 90', () => {
-  strictEqual(prettierConfig.printWidth, 90);
-});
+describe('index.js', () => {
+  it('printWidth should be 90', () => {
+    strictEqual(prettierConfig.printWidth, 90);
+  });
 
-test('tabWidth should be 2', () => {
-  strictEqual(prettierConfig.tabWidth, 2);
-});
+  it('tabWidth should be 2', () => {
+    strictEqual(prettierConfig.tabWidth, 2);
+  });
 
-test('useTabs should be false', () => {
-  strictEqual(prettierConfig.useTabs, false);
-});
+  it('useTabs should be false', () => {
+    strictEqual(prettierConfig.useTabs, false);
+  });
 
-test('semi should be true', () => {
-  strictEqual(prettierConfig.semi, true);
-});
+  it('semi should be true', () => {
+    strictEqual(prettierConfig.semi, true);
+  });
 
-test('singleQuote should be true', () => {
-  strictEqual(prettierConfig.singleQuote, true);
-});
+  it('singleQuote should be true', () => {
+    strictEqual(prettierConfig.singleQuote, true);
+  });
 
-test('quoteProps should be "as-needed"', () => {
-  strictEqual(prettierConfig.quoteProps, 'as-needed');
-});
+  it('quoteProps should be "as-needed"', () => {
+    strictEqual(prettierConfig.quoteProps, 'as-needed');
+  });
 
-test('jsxSingleQuote should be false', () => {
-  strictEqual(prettierConfig.jsxSingleQuote, false);
-});
+  it('jsxSingleQuote should be false', () => {
+    strictEqual(prettierConfig.jsxSingleQuote, false);
+  });
 
-test('trailingComma should be "all"', () => {
-  strictEqual(prettierConfig.trailingComma, 'all');
-});
+  it('trailingComma should be "all"', () => {
+    strictEqual(prettierConfig.trailingComma, 'all');
+  });
 
-test('bracketSpacing should be true', () => {
-  strictEqual(prettierConfig.bracketSpacing, true);
-});
+  it('bracketSpacing should be true', () => {
+    strictEqual(prettierConfig.bracketSpacing, true);
+  });
 
-test('bracketSameLine should be false', () => {
-  strictEqual(prettierConfig.bracketSameLine, false);
-});
+  it('bracketSameLine should be false', () => {
+    strictEqual(prettierConfig.bracketSameLine, false);
+  });
 
-test('arrowParens should be "avoid"', () => {
-  strictEqual(prettierConfig.arrowParens, 'avoid');
-});
+  it('arrowParens should be "avoid"', () => {
+    strictEqual(prettierConfig.arrowParens, 'avoid');
+  });
 
-test('proseWrap should be "never"', () => {
-  strictEqual(prettierConfig.proseWrap, 'never');
-});
+  it('proseWrap should be "never"', () => {
+    strictEqual(prettierConfig.proseWrap, 'never');
+  });
 
-test('htmlWhitespaceSensitivity should be "css"', () => {
-  strictEqual(prettierConfig.htmlWhitespaceSensitivity, 'css');
-});
+  it('htmlWhitespaceSensitivity should be "css"', () => {
+    strictEqual(prettierConfig.htmlWhitespaceSensitivity, 'css');
+  });
 
-test('endOfLine should be "lf"', () => {
-  strictEqual(prettierConfig.endOfLine, 'lf');
-});
+  it('endOfLine should be "lf"', () => {
+    strictEqual(prettierConfig.endOfLine, 'lf');
+  });
 
-test('singleAttributePerLine should be false', () => {
-  strictEqual(prettierConfig.singleAttributePerLine, false);
+  it('singleAttributePerLine should be false', () => {
+    strictEqual(prettierConfig.singleAttributePerLine, false);
+  });
 });
