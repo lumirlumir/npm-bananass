b9a33a7ccccf7765483b6ced2efb5d0dfe133c6a
루밀LuMir <rpfos@naver.com>
2025-01-10 23:25:27 +0900

    wip: `node.js`

--- packages/eslint-config-bananass/src/rules/node.js
+++ packages/eslint-config-bananass/src/rules/node.js
@@ -24,16 +24,16 @@ module.exports = {
   /**
    * Enforce either `module.exports` or `exports`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/exports-style': '',
 
   /**
    * Enforce the style of file extensions in import declarations.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/file-extension-in-import': '',
 
@@ -56,64 +56,64 @@ module.exports = {
   /**
    * Require correct usage of hashbang.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/hashbang': '',
 
   /**
    * Enforce Node.js-style error-first callback pattern is followed.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-callback-literal': '',
 
   /**
    * Disallow deprecated APIs.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-deprecated-api': '',
 
   /**
    * Disallow the assignment to `exports`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-exports-assign': '',
 
   /**
    * Disallow `import` declarations which import extraneous modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-extraneous-import': '',
 
   /**
    * Disallow `require()` expressions which import extraneous modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-extraneous-require': '',
 
   /**
    * Disallow `import` declarations which import non-existence modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-missing-import': '',
 
   /**
    * Disallow require() expressions which import non-existence modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-missing-require': '',
 
@@ -160,16 +160,16 @@ module.exports = {
   /**
    * Disallow specified modules when loaded by `import` declarations.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-restricted-import': '',
 
   /**
    * Disallow specified modules when loaded by `require`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-restricted-require': '',
 
@@ -184,136 +184,136 @@ module.exports = {
   /**
    * Disallow `bin` files that npm ignores.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unpublished-bin': '',
 
   /**
    * Disallow `import` declarations which import private modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unpublished-import': '',
 
   /**
    * Disallow `require()` expressions which import private modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unpublished-require': '',
 
   /**
    * Disallow unsupported ECMAScript built-ins on the specified version.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unsupported-features/es-builtins': '',
 
   /**
    * Disallow unsupported ECMAScript syntax on the specified version.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unsupported-features/es-syntax': '',
 
   /**
    * Disallow unsupported Node.js built-in APIs on the specified version.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/no-unsupported-features/node-builtins': '',
 
   /**
    * Enforce either `Buffer` or `require("buffer").Buffer`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/buffer': '',
 
   /**
    * Enforce either `console` or `require("console")`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/console': '',
 
   /**
    * Enforce either `process` or `require("process")`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/process': '',
 
   /**
    * Enforce either `TextDecoder` or `require("util").TextDecoder`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/text-decoder': '',
 
   /**
    * Enforce either `TextEncoder` or `require("util").TextEncoder`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/text-encoder': '',
 
   /**
    * Enforce either `URL` or `require("url").URL`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/url': '',
 
   /**
    * Enforce either `URLSearchParams` or `require("url").URLSearchParams`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-global/url-search-params': '',
 
   /**
    * Enforce using the `node:` protocol when importing Node.js builtin modules.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'prefer-node-protocol': '',
 
   /**
    * Enforce `require("dns").promises`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-promises/dns': '',
 
   /**
    * Enforce `require("fs").promises`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/prefer-promises/fs': '',
 
   /**
    * Require that `process.exit()` expressions use the same code path as `throw`.
    *
+   * @description This rule is not included in `airbnb-base`.
    * @link n: {@link }
-   * @link airbnb-base: {@link }
    */
   // 'n/process-exit-as-throw': '',
 };
