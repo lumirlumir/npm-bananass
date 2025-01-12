b721a1f7cbdb582c4a3fc3a1ddaa8778f62a780f
루밀LuMir <rpfos@naver.com>
2025-01-12 21:11:41 +0900

    test(eslint-config-bananass): update import rule tests and add node.js tests

--- packages/eslint-config-bananass/src/rules/import.test.js
+++ packages/eslint-config-bananass/src/rules/import.test.js
@@ -24,7 +24,7 @@ const prefix = 'import/';
 // Test
 // --------------------------------------------------------------------------------
 
-describe('All key values must start with `import/`.', () => {
+describe(`All key values must start with \`${prefix}\`.`, () => {
   it('import-helpful-warnings.js', () => {
     Object.keys(importHelpfulWarnings).forEach(key => {
       strictEqual(key.startsWith(prefix), true);
