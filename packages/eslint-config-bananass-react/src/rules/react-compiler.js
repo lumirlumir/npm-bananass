6722c83e84ab6f18afe12bf5215c568cf472652d
루밀LuMir <rpfos@naver.com>
2025-01-17 18:39:50 +0900

    wip

--- packages/eslint-config-bananass-react/src/rules/react-compiler.js
+++ packages/eslint-config-bananass-react/src/rules/react-compiler.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview This file follows:
  *
- * - The rules listed on 'eslint-plugin-react-compiler'.
+ * - The rules listed on `eslint-plugin-react-compiler`.
  *   - See, {@link https://github.com/facebook/react/tree/main/compiler/packages/eslint-plugin-react-compiler#eslint-plugin-react-compiler}.
  */
 
--- packages/eslint-config-bananass-react/src/rules/react-hooks.js
+++ packages/eslint-config-bananass-react/src/rules/react-hooks.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview This file follows:
  *
- * - The recommended rules listed on 'eslint-plugin-react-hooks'.
+ * - The rules listed on `eslint-plugin-react-hooks`.
  *   - See, {@link https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks#eslint-plugin-react-hooks}.
  */
 
@@ -13,14 +13,15 @@ module.exports = {
   /**
    * Verifies the list of dependencies for Hooks like `useEffect` and similar.
    *
-   * @link react-hooks: {@link https://github.com/facebook/react/blob/v19.0.0/packages/eslint-plugin-react-hooks/src/index.js#L18}
+   * @description This rule uses 'error' instead of 'warn'.
+   * @link https://github.com/facebook/react/blob/v19.0.0/packages/eslint-plugin-react-hooks/src/index.js#L18
    */
-  'react-hooks/exhaustive-deps': 'warn',
+  'react-hooks/exhaustive-deps': 'error',
 
   /**
    * Enforces the Rules of Hooks.
    *
-   * @link react-hooks: {@link https://github.com/facebook/react/blob/v19.0.0/packages/eslint-plugin-react-hooks/src/index.js#L17}
+   * @link https://github.com/facebook/react/blob/v19.0.0/packages/eslint-plugin-react-hooks/src/index.js#L17
    */
   'react-hooks/rules-of-hooks': 'error',
 };
