95e9e178718a93c7914a6d1b5408162a65af3163
루밀LuMir <rpfos@naver.com>
2025-01-19 23:54:03 +0900

    test(eslint-config-bananass-react): add tests for `react`, `react-hooks`, and `react-compiler` rules

---
+++ packages/eslint-config-bananass-react/src/rules/react-compiler.test.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Test for `react-compiler.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const { strictEqual } = require('node:assert');
+const { describe, it } = require('node:test');
+
+const reactCompiler = require('./react-compiler');
+
+// --------------------------------------------------------------------------------
+// Declaration
+// --------------------------------------------------------------------------------
+
+const prefix = 'react-compiler/';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe(`All key values must start with \`${prefix}\`.`, () => {
+  it('react-compiler.js', () => {
+    Object.keys(reactCompiler).forEach(key => {
+      strictEqual(key.startsWith(prefix), true);
+    });
+  });
+});
