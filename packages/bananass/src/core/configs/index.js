eff04c82020d0447c2fe3d9211844a42a5597b55
루밀LuMir <rpfos@naver.com>
2025-01-29 22:55:20 +0900

    wip: create `load-bananass-config-file.js`

--- packages/bananass/src/core/configs/index.js
+++ packages/bananass/src/core/configs/index.js
@@ -1,5 +1,11 @@
 import findBananassConfigFile from './find-bananass-config-file.js';
 import findPackageJsonFile from './find-package-json-file.js';
+import loadBananassConfigFile from './load-bananass-config-file.js';
 import loadPackageJsonFile from './load-package-json-file.js';
 
-export { findBananassConfigFile, findPackageJsonFile, loadPackageJsonFile };
+export {
+  findBananassConfigFile,
+  findPackageJsonFile,
+  loadBananassConfigFile,
+  loadPackageJsonFile,
+};
---
+++ packages/bananass/src/core/configs/load-bananass-config-file.js
@@ -0,0 +1,34 @@
+import path from 'node:path';
+import { createRequire } from 'node:module';
+
+import loadPackageJsonFile from './load-package-json-file.js';
+import { JS_EXT_ARRAY } from '../constants';
+
+export default async function loadBananassConfigFile(
+  pathToBananassConfigFile,
+  pathToPackageJsonFile,
+) {
+  const { type } = loadPackageJsonFile(pathToPackageJsonFile);
+  const ext = path.extname(pathToBananassConfigFile);
+
+  switch (ext) {
+    case '.js': {
+      if (type === 'module') {
+        return await import(`file://${pathToBananassConfigFile}`);
+      }
+
+      return createRequire(import.meta.url)(pathToBananassConfigFile);
+    }
+
+    case '.cjs':
+      return createRequire(import.meta.url)(pathToBananassConfigFile);
+
+    case '.mjs':
+      return await import(`file://${pathToBananassConfigFile}`);
+
+    default:
+      throw new Error(
+        `Invalid file extension: \`${ext}\`. Only ${JS_EXT_ARRAY.join(', ')} are supported.`,
+      );
+  }
+}
