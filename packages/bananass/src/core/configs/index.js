780bfcd46a8b8e2be6c761096fd0266afc26c2e8
루밀LuMir <rpfos@naver.com>
2025-01-29 21:15:16 +0900

    wip: create `load-package-json-file.js`

--- packages/bananass/src/core/configs/index.js
+++ packages/bananass/src/core/configs/index.js
@@ -1,4 +1,5 @@
 import findBananassConfigFile from './find-bananass-config-file';
 import findPackageJsonFile from './find-package-json-file';
+import loadPackageJsonFile from './load-package-json-file';
 
-export { findBananassConfigFile, findPackageJsonFile };
+export { findBananassConfigFile, findPackageJsonFile, loadPackageJsonFile };
