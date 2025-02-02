edf474c0847d894ac7649ac33679ec31e19c6733
루밀LuMir <rpfos@naver.com>
2025-02-02 22:58:59 +0900

    wip: update `constants.js`

--- packages/bananass/src/core/constants.js
+++ packages/bananass/src/core/constants.js
@@ -13,7 +13,9 @@ import { format, join } from 'node:path';
 // Declaration
 // --------------------------------------------------------------------------------
 
-const { name: packageName } = createRequire(import.meta.url)('../../package.json');
+const { description, homepage, name, version } = createRequire(import.meta.url)(
+  '../../package.json',
+);
 
 // --------------------------------------------------------------------------------
 // Export
@@ -23,25 +25,25 @@ const { name: packageName } = createRequire(import.meta.url)('../../package.json
 export const BAEKJOON_PROBLEM_NUMBER_MIN = 1_000;
 
 /* String */
-export const PACKAGE_NAME = packageName;
+export const PKG_DESCRIPTION = description;
+export const PKG_HOMEPAGE = homepage;
+export const PKG_NAME = name;
+export const PKG_VERSION = version;
 
 /* Array */
 // Array constants with `NAME` in their name must not include the extension.
 // Array constants with `BASE` in their name must include the extension.
 export const JS_EXT_ARRAY = Object.freeze(['.js', '.cjs', '.mjs']);
 export const PACKAGE_JSON_FILE_BASE_ARRAY = Object.freeze(['package.json']);
-export const ENTRY_DIR_NAME_ARRAY = Object.freeze([
-  packageName,
-  join('src', packageName),
-]);
-export const OUTPUT_DIR_NAME_ARRAY = Object.freeze([`.${packageName}`]);
+export const ENTRY_DIR_NAME_ARRAY = Object.freeze([name, join('src', name)]);
+export const OUTPUT_DIR_NAME_ARRAY = Object.freeze([`.${name}`]);
 export const BANANASS_CONFIG_FILE_NAME_ARRAY = Object.freeze([
-  `${packageName}.config`,
-  `.${packageName}rc`,
+  `${name}.config`,
+  `.${name}rc`,
 ]);
 export const BANANASS_CONFIG_FILE_BASE_ARRAY = Object.freeze(
-  BANANASS_CONFIG_FILE_NAME_ARRAY.flatMap(name =>
-    JS_EXT_ARRAY.map(ext => format({ name, ext })),
+  BANANASS_CONFIG_FILE_NAME_ARRAY.flatMap(configFileName =>
+    JS_EXT_ARRAY.map(ext => format({ name: configFileName, ext })),
   ),
 );
 export const WEBPACK_ENTRY_FILE_TEMPLATE_TYPE_ARRAY = Object.freeze([
