de031dbf49aa265d168429920fc4c6965ae6a05c
루밀LuMir <rpfos@naver.com>
2025-02-03 18:20:38 +0900

    wip: create `types.js`

---
+++ packages/bananass/src/core/types.js
@@ -0,0 +1,74 @@
+/**
+ * @fileoverview Define common types for input completion.
+ */
+
+// --------------------------------------------------------------------------------
+// Typedefs
+// --------------------------------------------------------------------------------
+
+/**
+ * @typedef {object} ConfigObject
+ * @property {ConfigObjectAddOptions} addOptions Add options.
+ * @property {ConfigObjectBuildOptions} buildOptions Build options.
+ * @property {ConfigObjectCleanOptions} cleanOptions Clean options.
+ * @property {ConfigObjectInfoOptions} infoOptions Info options.
+ * @property {ConfigObjectInitOptions} initOptions Init options.
+ * @property {ConfigObjectLintOptions} lintOptions Lint options.
+ * @property {ConfigObjectLoginOptions} loginOptions Login options.
+ * @property {ConfigObjectOpenOptions} openOptions Open options.
+ * @property {ConfigObjectRandomOptions} randomOptions Random options.
+ * @property {ConfigObjectRunOptions} runOptions Run options.
+ * @property {ConfigObjectSubmitOptions} submitOptions Submit options.
+ * @property {ConfigObjectTestcaseOptions} testcaseOptions Testcase options.
+ */
+
+/**
+ * @typedef {object} ConfigObjectAddOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectBuildOptions
+ * @property {boolean} clean Clean the output directory before emit.
+ * @property {boolean} debug Enable debug mode.
+ * @property {boolean} quiet Enable quiet mode.
+ */
+
+/**
+ * @typedef {object} ConfigObjectCleanOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectInfoOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectInitOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectLintOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectLoginOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectOpenOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectRandomOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectRunOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectSubmitOptions
+ */
+
+/**
+ * @typedef {object} ConfigObjectTestcaseOptions
+ */
