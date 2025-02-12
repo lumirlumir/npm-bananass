657d71a50d0032bdaa46b74ba57cefecd24a69de
루밀LuMir <rpfos@naver.com>
2025-02-12 14:42:08 +0900

    wip: `config-object-submit.js`

---
+++ packages/bananass/src/core/structs/config-object-submit/config-object-submit.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview `ConfigObjectSubmit` type struct.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { object } from 'superstruct';
+
+// --------------------------------------------------------------------------------
+// Typedefs
+// --------------------------------------------------------------------------------
+
+/**
+ * @typedef {import('../../types.js').ConfigObjectSubmit} ConfigObjectSubmit
+ * @typedef {import('superstruct').Struct<ConfigObjectSubmit>} ConfigObjectSubmitStruct
+ */
+
+// --------------------------------------------------------------------------------
+// Type Struct
+// --------------------------------------------------------------------------------
+
+/**
+ * `ConfigObjectSubmit` type struct.
+ *
+ * @type {ConfigObjectSubmitStruct}
+ */
+const ConfigObjectSubmit = object({});
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
+
+export default ConfigObjectSubmit;
