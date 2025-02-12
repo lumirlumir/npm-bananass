d994db3bb2b660c0cc3659fdafcf9ebe3c392e60
루밀LuMir <rpfos@naver.com>
2025-02-13 01:22:50 +0900

    refactor(bananass-utils-console): improve types and JSDoc (#152)
    This pull request includes several changes to the
    `bananass-utils-console` package, focusing on restructuring exports,
    adding type definitions, and updating TypeScript configuration.
    Additionally, there are minor updates to other related files to remove
    temporary workarounds and improve type checking.
    
    ### Restructuring Exports:
    * Removed the main export entry from
    `packages/bananass-utils-console/package.json` to focus on specific
    module exports.
    * Deleted the entry file `packages/bananass-utils-console/src/index.js`
    which previously exported `createLogger`, `createSpinner`, and `theme`.
    
    ### Adding Type Definitions:
    * Added JSDoc typedefs for `Options` in
    `packages/bananass-utils-console/src/logger.js` to improve type clarity.
    * Updated the `Logger` class in
    `packages/bananass-utils-console/src/logger.js` to include type
    annotations for private properties and method parameters.
    [[1]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R32-R45)
    [[2]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R62-R73)
    [[3]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R102-R114)
    [[4]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R143-R151)
    [[5]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R167-R171)
    [[6]](diffhunk://#diff-ca61110b96c84d07f62507411be0440c90521177060be76fd4fddcaeb122cd33R182-R188)
    
    ### TypeScript Configuration:
    * Updated `tsconfig.json` in `packages/bananass-utils-console` to allow
    synthetic default imports, check JavaScript files, and skip library
    checks.
    
    ### Minor Updates:
    * Added `// @ts-nocheck` comment in
    `packages/bananass-utils-console/src/spinner.js` to temporarily disable
    type checking.
    * Removed `@ts-ignore` comments from `bananass` package files where type
    declarations were previously ignored.
    [[1]](diffhunk://#diff-0ce95c8c9e2a9153c60042f1f9db2f62324e019f7980eb2e8ee8a5001fd6e34bL71)
    [[2]](diffhunk://#diff-51296f54e1d3e1c2abe3893ec66ff745811830d67cd373dcfdbe56986e7df4b8L51)
    [[3]](diffhunk://#diff-aed2769c5bf3d27e1aa5d4b258ebffc32608a930d0f9f6a9d4c31e6416de2122L5-L6)

--- packages/bananass/src/core/fs/find-root-dir/find-root-dir.js
+++ packages/bananass/src/core/fs/find-root-dir/find-root-dir.js
@@ -2,8 +2,6 @@
  * @fileoverview Find root directory path based on the existence of `package.json`.
  */
 
-// @ts-nocheck -- TODO: Delete this line later.
-
 // --------------------------------------------------------------------------------
 // Import
 // --------------------------------------------------------------------------------
