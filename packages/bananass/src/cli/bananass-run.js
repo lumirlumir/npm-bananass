808ec6c1629d63e933c6038871c12f44e50a4657
루밀LuMir <rpfos@naver.com>
2025-02-17 18:47:01 +0900

    wip: update `bananass-run.js`

--- packages/bananass/src/cli/bananass-run.js
+++ packages/bananass/src/cli/bananass-run.js
@@ -6,7 +6,19 @@
 // Import
 // --------------------------------------------------------------------------------
 
-import { warning } from 'bananass-utils-console/theme';
+import logger from 'bananass-utils-console/logger';
+
+import { run as runCmd } from '../commands/index.js';
+import { configLoader, defaultConfigObject } from '../core/conf/index.js';
+
+import { run as runDesc } from '../core/cli/descriptions.js';
+import { problems as problemsArg } from '../core/cli/arguments.js';
+import {
+  cwd as cwdOpt,
+  entryDir as entryDirOpt,
+  debug as debugOpt,
+  quiet as quietOpt,
+} from '../core/cli/options.js';
 
 // --------------------------------------------------------------------------------
 // Typedefs
@@ -14,6 +26,7 @@ import { warning } from 'bananass-utils-console/theme';
 
 /**
  * @typedef {import('commander').Command} Command
+ * @typedef {import('../core/types.js').ConfigObject} ConfigObject
  */
 
 // --------------------------------------------------------------------------------
@@ -26,5 +39,40 @@ import { warning } from 'bananass-utils-console/theme';
  * @param {Command} program The `commander` package's `program`.
  */
 export default function run(program) {
-  program.command('run').description(warning('TODO: Working in progress...🚧', false));
+  program
+    .command('run')
+    .description(runDesc)
+    .argument(...problemsArg)
+    .option(...cwdOpt)
+    .option(...entryDirOpt)
+    .option(...debugOpt)
+    .option(...quietOpt)
+    .action(async (problems, options, command) => {
+      const { cwd, entryDir, debug, quiet } = options;
+
+      /** @type {ConfigObject} */
+      const cliConfigObject = {
+        cwd,
+        entryDir,
+        console: {
+          debug,
+          quiet,
+        },
+      };
+
+      const { config: configObject } = await configLoader({
+        cliConfigObject,
+        defaultConfigObject,
+      });
+
+      logger(configObject.console)
+        .debug('command:', command.name())
+        .debug('problems:', problems)
+        .debug('cli options:', options)
+        .debug('cli config object:', cliConfigObject)
+        .debug('config object:', configObject)
+        .eol();
+
+      await runCmd(problems, configObject);
+    });
 }
