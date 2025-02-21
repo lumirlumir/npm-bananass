bc95765325603a7f5fa4cb4fa940bb59a8e715df
루밀LuMir <rpfos@naver.com>
2025-02-21 18:18:28 +0900

    wip

--- websites/eslint-config-bananass/config.js
+++ websites/eslint-config-bananass/config.js
@@ -1,3 +1,7 @@
 const bananass = require('eslint-config-bananass');
 
-module.exports = [bananass.configs.js, bananass.configs.jsx.react];
+module.exports = [
+  bananass.configs.js,
+  bananass.configs.jsx.react,
+  bananass.configs.jsx.next,
+];
