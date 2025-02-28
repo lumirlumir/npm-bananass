52c3e8bbe7a272cf4796d01e67530354d762d0eb
루밀LuMir <rpfos@naver.com>
2025-03-01 01:08:13 +0900

    wip

--- packages/bananass/src/commands/bananass-bug/bug.js
+++ packages/bananass/src/commands/bananass-bug/bug.js
@@ -55,7 +55,7 @@ export default async function bug(configObject) {
   // CLI Animation
   // ------------------------------------------------------------------------------
 
-  logger.log(() => spinner.start(bananass('Opening in a browser...')));
+  logger.log(() => spinner.start(bananass('Opening in a browser...'))); // TODO: Set `showIcon` true
 
   // ------------------------------------------------------------------------------
   // Open
