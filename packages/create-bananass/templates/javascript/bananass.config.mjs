cf859c10dc4aed9b9535ae853268647034bb978a
루밀LuMir <rpfos@naver.com>
2025-03-01 00:24:30 +0900

    wip: create `bananass.config.mjs`

---
+++ packages/create-bananass/templates/javascript/bananass.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('bananass').ConfigObject} */
+export default {
+  // Add your custom Bananass configuration here.
+  //
+  // For example:
+  //
+  // entryDir: 'bananass',
+  // outDir: '.bananass',
+  // browser: {
+  //   secretMode: true,
+  // }
+  // build: {
+  //   templateType: 'rl',
+  // }
+  // ...
+};
+
+// Take a look at https://bananass.lumir.page
+// which lists all the available configurations and options.
