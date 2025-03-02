1b95e42795afb4fd3a0d8bbcdf75887536a22c80
루밀LuMir <rpfos@naver.com>
2025-03-02 20:34:13 +0900

    wip: `examples-solutions-bananass-mts`

---
+++ examples/solutions-bananass-mts/bananass/29751.ts
@@ -0,0 +1,31 @@
+import type { Testcases, Solution, SolutionWithTestcases } from 'bananass';
+
+const testcases = [
+  {
+    input: '1 1',
+    output: '0.5',
+  },
+  {
+    input: '2 3',
+    output: '3.0',
+  },
+  {
+    input: '4 5',
+    output: '10.0',
+  },
+] satisfies Testcases;
+
+const solution: Solution = input => {
+  const [W, H] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  const width = (W * H) / 2;
+
+  return width.toFixed(1); // `toFixed` returns a string.
+};
+
+export default globalThis.IS_PROD
+  ? { solution }
+  : ({ solution, testcases } satisfies SolutionWithTestcases);
