0944b9e952ea591ffa4c31f91acfec6e1d281a08
루밀LuMir <rpfos@naver.com>
2025-03-02 20:27:08 +0900

    wip: `examples-solutions-bananass-cts`

---
+++ examples/solutions-bananass-cts/bananass/1000.ts
@@ -0,0 +1,33 @@
+import type {
+  Testcases,
+  Solution,
+  SolutionWithTestcases,
+} from 'bananass' with { 'resolution-mode': 'require' };
+
+const testcases = [
+  {
+    input: '1 2',
+    output: '3',
+  },
+  {
+    input: '3 4',
+    output: '7',
+  },
+  {
+    input: '5 6',
+    output: '11',
+  },
+] satisfies Testcases;
+
+const solution: Solution = input => {
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return a + b;
+};
+
+module.exports = globalThis.IS_PROD
+  ? { solution }
+  : ({ solution, testcases } satisfies SolutionWithTestcases);
---
+++ examples/solutions-bananass-cts/bananass/1001.ts
@@ -0,0 +1,33 @@
+import type {
+  Testcases,
+  Solution,
+  SolutionWithTestcases,
+} from 'bananass' with { 'resolution-mode': 'require' };
+
+const testcases = [
+  {
+    input: '3 2\n',
+    output: 1,
+  },
+  {
+    input: '5 6\n',
+    output: -1,
+  },
+  {
+    input: '7 7\n',
+    output: 0,
+  },
+] satisfies Testcases;
+
+const solution: Solution = input => {
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return a - b;
+};
+
+module.exports = globalThis.IS_PROD
+  ? { solution }
+  : ({ solution, testcases } satisfies SolutionWithTestcases);
