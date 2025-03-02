dad8769c8f8b266b8592d041e26332d7fe9aed1c
루밀LuMir <rpfos@naver.com>
2025-03-02 20:29:57 +0900

    wip: `examples-solutions-bananass-mjs`

---
+++ examples/solutions-bananass-mjs/bananass/1000.js
@@ -0,0 +1,25 @@
+const testcases = [
+  {
+    input: '1 2',
+    output: '3',
+  },
+  {
+    input: '3 4',
+    output: '7',
+  },
+  {
+    input: '5 6',
+    output: '11',
+  },
+];
+
+function solution(input) {
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return a + b;
+}
+
+export default globalThis.IS_PROD ? { solution } : { solution, testcases };
