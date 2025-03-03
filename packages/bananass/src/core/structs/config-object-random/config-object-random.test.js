4065a7338dfe305b2311895009de604f3f5586d6
루밀LuMir <rpfos@naver.com>
2025-03-03 20:54:18 +0900

    wip: `config-object-*` tests

--- packages/bananass/src/core/structs/config-object-random/config-object-random.test.js
+++ packages/bananass/src/core/structs/config-object-random/config-object-random.test.js
@@ -1,1 +1,34 @@
-// TODO
+/**
+ * @fileoverview Test for `config-object-random.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { strictEqual } from 'node:assert';
+import { describe, it } from 'node:test';
+
+import ConfigObjectRandom from './config-object-random.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('config-object-random.js', () => {
+  // true
+  it('should return true for an empty object', () => {
+    const configObjectRandom = {};
+
+    strictEqual(ConfigObjectRandom.is(configObjectRandom), true);
+  });
+
+  // false
+  it('should return false for an unknown property', () => {
+    const configObjectRandom = {
+      unknownProperty: 'Hello, World!',
+    };
+
+    strictEqual(ConfigObjectRandom.is(configObjectRandom), false);
+  });
+});
--- packages/bananass/src/core/structs/config-object-testcase/config-object-testcase.test.js
+++ packages/bananass/src/core/structs/config-object-testcase/config-object-testcase.test.js
@@ -1,1 +1,34 @@
-// TODO
+/**
+ * @fileoverview Test for `config-object-testcase.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { strictEqual } from 'node:assert';
+import { describe, it } from 'node:test';
+
+import ConfigObjectTestcase from './config-object-testcase.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('config-object-testcase.js', () => {
+  // true
+  it('should return true for an empty object', () => {
+    const configObjectTestcase = {};
+
+    strictEqual(ConfigObjectTestcase.is(configObjectTestcase), true);
+  });
+
+  // false
+  it('should return false for an unknown property', () => {
+    const configObjectTestcase = {
+      unknownProperty: 'Hello, World!',
+    };
+
+    strictEqual(ConfigObjectTestcase.is(configObjectTestcase), false);
+  });
+});
