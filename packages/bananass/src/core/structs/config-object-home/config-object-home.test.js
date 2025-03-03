095d09f8d7ac860724f9b6064601fbc82fb386fb
루밀LuMir <rpfos@naver.com>
2025-03-03 23:10:25 +0900

    wip: `config-object-*` tests

--- packages/bananass/src/core/structs/config-object-home/config-object-home.test.js
+++ packages/bananass/src/core/structs/config-object-home/config-object-home.test.js
@@ -1,1 +1,34 @@
-// TODO
+/**
+ * @fileoverview Test for `config-object-home.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { strictEqual } from 'node:assert';
+import { describe, it } from 'node:test';
+
+import ConfigObjectHome from './config-object-home.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('config-object-home.js', () => {
+  // true
+  it('should return true for an empty object', () => {
+    const configObjectHome = {};
+
+    strictEqual(ConfigObjectHome.is(configObjectHome), true);
+  });
+
+  // false
+  it('should return false for an unknown property', () => {
+    const configObjectHome = {
+      unknownProperty: 'Hello, World!',
+    };
+
+    strictEqual(ConfigObjectHome.is(configObjectHome), false);
+  });
+});
