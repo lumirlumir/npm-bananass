eec186fcdb07e89b4d1f7285b4876aaae33cef1f
루밀LuMir <rpfos@naver.com>
2025-03-08 01:26:20 +0900

    wip

--- packages/bananass/src/commands/bananass-discussion/discussion.test.js
+++ packages/bananass/src/commands/bananass-discussion/discussion.test.js
@@ -1,1 +1,93 @@
-// TODO
+/**
+ * @fileoverview Test for `discussion.js`.
+ * @see https://nodejs.org/en/learn/test-runner/mocking#modules--units
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { match, strictEqual, rejects } from 'node:assert';
+import { describe, it, before, afterEach, mock } from 'node:test';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('discussion.js', () => {
+  // ------------------------------------------------------------------------------
+  // Mocking
+  // ------------------------------------------------------------------------------
+
+  let discussion;
+  const openDefaultExportMock = mock.fn(() => {});
+
+  before(async () => {
+    const openNamedExports = await import('open').then(({ default: _, ...rest }) => rest);
+
+    // Mocking `open` package.
+    mock.module('open', {
+      defaultExport: openDefaultExportMock,
+      namedExports: openNamedExports,
+    });
+
+    // Mocking `discussion` function.
+    discussion = await import('./discussion.js').then(({ default: d }) => d);
+  });
+
+  afterEach(() => {
+    openDefaultExportMock.mock.resetCalls();
+  });
+
+  // ------------------------------------------------------------------------------
+  // Test
+  // ------------------------------------------------------------------------------
+
+  describe('should use default parameters correctly', () => {
+    it('should use default parameters when no arguments are provided', async () => {
+      await discussion(); // Same with `await doesNotReject(() => discussion());`.
+    });
+
+    it('should use default parameters when empty object (`{}`) is provided', async () => {
+      await discussion({});
+    });
+
+    it('should use default parameters when only browser object is provided', async () => {
+      await discussion({ browser: {} });
+    });
+
+    it('should use default parameters when only console object is provided', async () => {
+      await discussion({ console: {} });
+    });
+
+    it('should use default parameters when both empty objects are provided', async () => {
+      await discussion({ browser: {}, console: {} });
+    });
+
+    it('should handle mixed valid and invalid values', async () => {
+      await discussion({ browser: { browser: 'chrome' }, console: { quiet: true } });
+    });
+  });
+
+  describe('should work as expected', () => {
+    it('should reject when invalid values are provided', async () => {
+      await rejects(() => discussion({ invalid: 'invalid' }));
+    });
+
+    it('should open the github discussions in a browser', async () => {
+      await discussion();
+
+      strictEqual(openDefaultExportMock.mock.callCount(), 1); // Should open the browser.
+      match(openDefaultExportMock.mock.calls[0].arguments[0], /discussions/); // Should open the github discussions.
+    });
+
+    it('should throw an error when browser opening fails for some reason', async () => {
+      openDefaultExportMock.mock.mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      await rejects(() => discussion(), { name: 'Error' });
+      strictEqual(openDefaultExportMock.mock.callCount(), 1); // Check the number of calls (whether `open` was called even if an error occurred)
+    });
+  });
+});
--- packages/bananass/src/commands/bananass-repo/repo.test.js
+++ packages/bananass/src/commands/bananass-repo/repo.test.js
@@ -1,1 +1,93 @@
-// TODO
+/**
+ * @fileoverview Test for `repo.js`.
+ * @see https://nodejs.org/en/learn/test-runner/mocking#modules--units
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { match, strictEqual, rejects } from 'node:assert';
+import { describe, it, before, afterEach, mock } from 'node:test';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('repo.js', () => {
+  // ------------------------------------------------------------------------------
+  // Mocking
+  // ------------------------------------------------------------------------------
+
+  let repo;
+  const openDefaultExportMock = mock.fn(() => {});
+
+  before(async () => {
+    const openNamedExports = await import('open').then(({ default: _, ...rest }) => rest);
+
+    // Mocking `open` package.
+    mock.module('open', {
+      defaultExport: openDefaultExportMock,
+      namedExports: openNamedExports,
+    });
+
+    // Mocking `repo` function.
+    repo = await import('./repo.js').then(({ default: d }) => d);
+  });
+
+  afterEach(() => {
+    openDefaultExportMock.mock.resetCalls();
+  });
+
+  // ------------------------------------------------------------------------------
+  // Test
+  // ------------------------------------------------------------------------------
+
+  describe('should use default parameters correctly', () => {
+    it('should use default parameters when no arguments are provided', async () => {
+      await repo(); // Same with `await doesNotReject(() => repo());`.
+    });
+
+    it('should use default parameters when empty object (`{}`) is provided', async () => {
+      await repo({});
+    });
+
+    it('should use default parameters when only browser object is provided', async () => {
+      await repo({ browser: {} });
+    });
+
+    it('should use default parameters when only console object is provided', async () => {
+      await repo({ console: {} });
+    });
+
+    it('should use default parameters when both empty objects are provided', async () => {
+      await repo({ browser: {}, console: {} });
+    });
+
+    it('should handle mixed valid and invalid values', async () => {
+      await repo({ browser: { browser: 'chrome' }, console: { quiet: true } });
+    });
+  });
+
+  describe('should work as expected', () => {
+    it('should reject when invalid values are provided', async () => {
+      await rejects(() => repo({ invalid: 'invalid' }));
+    });
+
+    it('should open the github repository in a browser', async () => {
+      await repo();
+
+      strictEqual(openDefaultExportMock.mock.callCount(), 1); // Should open the browser.
+      match(openDefaultExportMock.mock.calls[0].arguments[0], /lumirlumir\/npm-bananass/); // Should open the github repository.
+    });
+
+    it('should throw an error when browser opening fails for some reason', async () => {
+      openDefaultExportMock.mock.mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      await rejects(() => repo(), { name: 'Error' });
+      strictEqual(openDefaultExportMock.mock.callCount(), 1); // Check the number of calls (whether `open` was called even if an error occurred)
+    });
+  });
+});
