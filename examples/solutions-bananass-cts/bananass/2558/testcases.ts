29a1b80c4286e9372fefb8b4055bc02717536df1
루밀LuMir <rpfos@naver.com>
2025-03-09 23:00:29 +0900

    wip: cts

---
+++ examples/solutions-bananass-cts/bananass/2558/testcases.ts
@@ -0,0 +1,12 @@
+import type { Testcases } from 'bananass' with { 'resolution-mode': 'require' };
+
+module.exports = [
+  {
+    input: '1\n2\n',
+    output: '3',
+  },
+  {
+    input: '3\n4\n',
+    output: '7',
+  },
+] satisfies Testcases;
