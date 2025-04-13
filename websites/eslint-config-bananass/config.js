a6763f1c325ff45f0939af693f2e8e39ef79c1e3
루밀LuMir <rpfos@naver.com>
2025-04-13 22:21:38 +0900

    wip

--- websites/eslint-config-bananass/config.js
+++ websites/eslint-config-bananass/config.js
@@ -3,9 +3,9 @@ const bananass = require('eslint-config-bananass');
 /** @type {import("eslint").Linter.Config[]} */
 module.exports = [
   bananass.configs.js,
-  bananass.configs.jsx.react,
-  bananass.configs.jsx.next,
+  bananass.configs.jsxReact,
+  bananass.configs.jsxNext,
   bananass.configs.ts,
-  bananass.configs.tsx.react,
-  bananass.configs.tsx.next,
+  bananass.configs.tsxReact,
+  bananass.configs.tsxNext,
 ];
