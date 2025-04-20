052acfaa7ce6e75431b27da6338bf1b0510525ca
루밀LuMir <rpfos@naver.com>
2025-04-20 23:48:07 +0900

    wip: add tests

---
+++ tests/e2e/bananass-build/fixtures/mjs/bananass/3000.js
@@ -0,0 +1,10 @@
+export default function solution(input) {
+  const test = /(?i:a)a/.test('aa');
+
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return test ? a + b : a + b;
+}
