99fcb147b0115975b8719bab1bfca6e92e26f6ee
루밀LuMir <rpfos@naver.com>
2025-04-22 20:20:51 +0900

    wip

---
+++ tests/e2e/bananass-build/fixtures/cjs/bananass/3003.js
@@ -0,0 +1,12 @@
+function solution(input) {
+  const obj = { prop: 1 };
+
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return Object.hasOwn(obj, 'prop') ? a + b : a - b;
+}
+
+module.exports = { solution };
