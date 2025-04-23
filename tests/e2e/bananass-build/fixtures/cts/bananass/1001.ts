b8e21928f137c26744efd307db6a0805f9c0f607
루밀LuMir <rpfos@naver.com>
2025-04-23 22:46:07 +0900

    wip

---
+++ tests/e2e/bananass-build/fixtures/cts/bananass/1001.ts
@@ -0,0 +1,12 @@
+import type { Input, Output } from '../types';
+
+function solution(input: Input): Output {
+  const [a, b] = input
+    .trim()
+    .split(' ')
+    .map(val => Number(val));
+
+  return a + b;
+}
+
+module.exports = { solution };
