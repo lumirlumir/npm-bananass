844bcf096e1b7dfa5e37fc58383a0371b5830018
루밀LuMir <rpfos@naver.com>
2025-04-28 00:43:54 +0900

    test(tests-e2e): create e2e tests for `create-bananass`

---
+++ tests/e2e/create-bananass/cli.test.js
@@ -0,0 +1,71 @@
+/**
+ * @fileoverview E2E tests for `create-bananass` CLI command.
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { ok, strictEqual, match } from 'node:assert';
+import { describe, it, afterEach } from 'node:test';
+import { stripVTControlCharacters as stripAnsi } from 'node:util';
+import { spawnSync } from 'node:child_process';
+import { resolve } from 'node:path';
+import { existsSync, rmSync, readFileSync } from 'node:fs';
+
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
+const tempDirName = 'temp';
+const outDir = resolve(import.meta.dirname, tempDirName);
+const successMessage = /Successfully created a new Bananass framework project!/;
+
+/** @param {string[]} [args] Command line arguments. */
+function runCreateBananass(args = []) {
+  const { status, stderr } = spawnSync(
+    'npx',
+    ['create-bananass', `create-bananass/${tempDirName}`, ...args],
+    {
+      cwd: import.meta.dirname,
+      // If there is no interactive handling logic using `isInteractive()` in `cli.js`,
+      // `consola` will throw an error when `input` is passed to `spawnSync`.
+      // `spawnSync` assumes a non-interactive environment.
+      input: '',
+      encoding: 'utf-8',
+      shell: true,
+      timeout: 10_000, // 10 seconds.
+    },
+  );
+
+  return { status, stderr: stripAnsi(stderr).trim() };
+}
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+afterEach(() => {
+  // Clean up the output directory after each test
+  if (existsSync(outDir)) rmSync(outDir, { recursive: true, force: true });
+});
+
+describe('cli', () => {
+  describe('should create a correct project using templates', () => {
+    it('should create a JavaScript ESM project', () => {
+      const result = runCreateBananass(['--skip-vsc', '--skip-git', '--skip-install']);
+      const packageJson = JSON.parse(
+        readFileSync(resolve(outDir, 'package.json'), 'utf-8'),
+      );
+
+      strictEqual(result.status, 0);
+      match(result.stderr, successMessage);
+
+      strictEqual(packageJson.type, 'module');
+      strictEqual(packageJson.name, 'create-bananass-javascript-esm');
+
+      ok(existsSync(resolve(outDir, '.gitignore')));
+      ok(existsSync(resolve(outDir, 'bananass.config.mjs')));
+    });
+  });
+});
