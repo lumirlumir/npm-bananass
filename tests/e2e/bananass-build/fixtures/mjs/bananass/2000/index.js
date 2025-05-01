cf1f009489e3d3fc29ebd3f287e3aebf15c57f7f
루밀LuMir <rpfos@naver.com>
2025-05-01 22:22:30 +0900

    wip

--- tests/e2e/bananass-build/fixtures/mjs/bananass/2000/index.js
+++ tests/e2e/bananass-build/fixtures/mjs/bananass/2000/index.js
@@ -1,4 +1,4 @@
-import solution from './solution.js';
+import solution from './solution.js'; // NOTE: Extension is required in ESM
 import testcases from './testcases.js';
 
 export default { solution, testcases };
