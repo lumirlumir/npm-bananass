3ad519473061151c01f04c2ff7fdc303c963e9df
루밀LuMir <rpfos@naver.com>
2025-05-06 22:18:35 +0900

    wip: 2420

---
+++ examples/solutions-bananass-cts/bananass/2420.ts
@@ -0,0 +1,24 @@
+import type { Testcases, Input, Output } from 'bananass';
+
+const testcases = [
+  {
+    input: '-2 5\n',
+    output: 7,
+  },
+  {
+    input: '5 3\n',
+    output: 2,
+  },
+  {
+    input: '-5 -3\n',
+    output: 2,
+  },
+] satisfies Testcases;
+
+function solution(input: Input): Output {
+  const [N, M] = input.trim().split(' ').map(Number);
+
+  return Math.abs(N - M);
+}
+
+module.exports = { testcases, solution };
---
+++ examples/solutions-bananass-mjs/bananass/2420.js
@@ -0,0 +1,22 @@
+const testcases = [
+  {
+    input: '-2 5\n',
+    output: 7,
+  },
+  {
+    input: '5 3\n',
+    output: 2,
+  },
+  {
+    input: '-5 -3\n',
+    output: 2,
+  },
+];
+
+function solution(input) {
+  const [N, M] = input.trim().split(' ').map(Number);
+
+  return Math.abs(N - M);
+}
+
+export default { testcases, solution };
