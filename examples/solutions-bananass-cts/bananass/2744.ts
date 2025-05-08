e2f71d05632446b287ec13a574f8117b6ba00cba
루밀LuMir <rpfos@naver.com>
2025-05-08 22:56:15 +0900

    wip: 2744

---
+++ examples/solutions-bananass-cts/bananass/2744.ts
@@ -0,0 +1,30 @@
+import type { Testcases, Input, Output } from 'bananass';
+
+const testcases = [
+  {
+    input: 'WrongAnswer',
+    output: 'wRONGaNSWER',
+  },
+  {
+    input: 'Abc',
+    output: 'aBC',
+  },
+] satisfies Testcases;
+
+function solution(input: Input): Output {
+  let output = '';
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (char.toLowerCase() === char) {
+      output += char.toUpperCase(); // Lowercase
+    } else {
+      output += char.toLowerCase(); // Uppercase
+    }
+  }
+
+  return output;
+}
+
+module.exports = { testcases, solution };
