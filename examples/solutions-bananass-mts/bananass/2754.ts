20fbb8222ba24736cfa8bfa72a75857871747e6f
루밀LuMir <rpfos@naver.com>
2025-05-09 23:19:56 +0900

    wip: 2754

---
+++ examples/solutions-bananass-mts/bananass/2754.ts
@@ -0,0 +1,34 @@
+import type { Testcases, Input, Output } from 'bananass';
+
+const testcases = [
+  {
+    input: 'A0',
+    output: '4.0',
+  },
+  {
+    input: 'A+',
+    output: '4.3',
+  },
+] satisfies Testcases;
+
+const grade = {
+  'A+': '4.3',
+  A0: '4.0',
+  'A-': '3.7',
+  'B+': '3.3',
+  B0: '3.0',
+  'B-': '2.7',
+  'C+': '2.3',
+  C0: '2.0',
+  'C-': '1.7',
+  'D+': '1.3',
+  D0: '1.0',
+  'D-': '0.7',
+  F: '0.0',
+};
+
+function solution(input: Input): Output {
+  return grade[input.trim()];
+}
+
+export default { testcases, solution };
