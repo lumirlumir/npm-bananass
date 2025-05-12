14983346df46b45c282c037cf8d1b5dec25ae53a
SukJu Hong <ehensnfl@gmail.com>
2025-05-12 19:38:28 +0900

    docs(*): add example for BOJ Problem 28702 (FizzBuzz) (#433)
    # Summary
    This PR adds:
    - An example input/output case for BOJ Problem 28702 – FizzBuzz
    
    # Details
    The example is added in all supported formats:
    - CommonJS (.cjs)
    - ES Module (.mjs)
    - TypeScript CommonJS (.cts)
    - TypeScript ES Module (.mts)
    - The explanation is written in both Korean and English documents
    
    # Screenshots
    
    
    ![localhost_5173_en_solutions_baekjoon_28702](https://github.com/user-attachments/assets/504f6c17-04e2-4d05-b7ef-06a26b41a630)
    
    ---------
    
    Co-authored-by: 루밀LuMir <rpfos@naver.com>

---
+++ examples/solutions-bananass-cts/bananass/28702.ts
@@ -0,0 +1,51 @@
+import type { Input, Output, Testcases } from 'bananass';
+
+const testcases = [
+  {
+    input: `Fizz
+Buzz
+11`,
+    output: `Fizz`,
+  },
+  {
+    input: `980803
+980804
+FizzBuzz`,
+    output: `980806`,
+  },
+  {
+    input: `12
+Fizz
+Buzz`,
+    output: `FizzBuzz`,
+  },
+  {
+    input: `17
+Fizz
+19`,
+    output: `Buzz`,
+  },
+] satisfies Testcases;
+
+function solution(input: Input): Output {
+  let result = 0;
+  const splitedInput = input.trim().split('\n');
+
+  splitedInput.forEach((str, idx) => {
+    if (parseInt(str, 10) === Number(str)) {
+      result = parseInt(str, 10) + 3 - idx;
+    }
+  });
+
+  if (result % 3 === 0 && result % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (result % 3 === 0) {
+    return 'Fizz';
+  } else if (result % 5 === 0) {
+    return 'Buzz';
+  }
+
+  return result;
+}
+
+module.exports = { solution, testcases };
