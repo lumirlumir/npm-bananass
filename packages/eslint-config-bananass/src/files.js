8a423f76a3cb484249c32a343e791203279db094
루밀LuMir <rpfos@naver.com>
2025-09-23 15:13:07 +0900

    wip

--- packages/eslint-config-bananass/src/files.js
+++ packages/eslint-config-bananass/src/files.js
@@ -13,3 +13,14 @@ export const jsx = /** @type {const} */ (['**/*.jsx']);
 export const ts = /** @type {const} */ (['**/*.ts', '**/*.cts', '**/*.mts']);
 
 export const tsx = /** @type {const} */ (['**/*.tsx']);
+
+export const json = /** @type {const} */ (['**/*.json']);
+
+export const jsonc = /** @type {const} */ ([
+  '**/*.jsonc',
+  '**/.vscode/*.json',
+  '**/jsconfig.json',
+  '**/tsconfig.json',
+]);
+
+export const json5 = /** @type {const} */ (['**/*.json5']);
