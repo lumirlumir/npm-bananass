7fd4ee419a267dcfdad5ac4c616d6e205cfd012d
루밀LuMir <rpfos@naver.com>
2025-09-27 23:02:22 +0900

    fix(eslint-config-bananass): add additional JSON config ignores for `jsconfig` and `tsconfig` (#721)
    This pull request adds support for ignoring additional configuration
    files in the ESLint ignore list, specifically files that match wildcard
    patterns for `jsconfig` and `tsconfig`. This will help ensure that all
    relevant configuration files are excluded from linting.
    
    Configuration ignore pattern updates:
    
    * Added `**/jsconfig.*.json` and `**/tsconfig.*.json` to the ignore list
    in `packages/eslint-config-bananass/src/ignores.js`, allowing ESLint to
    ignore all variants of these config files.

--- packages/eslint-config-bananass/src/ignores.js
+++ packages/eslint-config-bananass/src/ignores.js
@@ -12,5 +12,7 @@ export const json = /** @type {const} */ ([
   'package-lock.json',
   '**/.vscode/*.json',
   '**/jsconfig.json',
+  '**/jsconfig.*.json',
   '**/tsconfig.json',
+  '**/tsconfig.*.json',
 ]);
