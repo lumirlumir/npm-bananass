79a78ce2e0f1a3693a27fdd17f15d2ee532652d3
루밀LuMir <rpfos@naver.com>
2025-10-16 15:21:00 +0900

    wip

--- packages/eslint-config-bananass/src/rules/stylistic-js/stylistic-js.js
+++ packages/eslint-config-bananass/src/rules/stylistic-js/stylistic-js.js
@@ -2,26 +2,33 @@
  * @fileoverview This file follows:
  *
  * - The order of the rules listed in the JavaScript section on 'ESLint Stylistic'.
- *   - See, {@link https://eslint.style/packages/js#rules}.
+ *   - @see https://eslint.style/packages/js#rules
  *
  * - The best practices outlined in `eslint-config-airbnb-base@19.0.4`.
- *   - See, {@link https://github.com/airbnb/javascript/tree/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules}.
+ *   - @see https://github.com/airbnb/javascript/tree/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules
  *
  * - The rules disabled by `eslint-config-prettier@9.1.0`.
- *   - See, {@link https://github.com/prettier/eslint-config-prettier/blob/v9.1.0/index.js}.
+ *   - @see https://github.com/prettier/eslint-config-prettier/blob/v9.1.0/index.js
+ */
+
+// --------------------------------------------------------------------------------
+// Typedef
+// --------------------------------------------------------------------------------
+
+/**
+ * @import { Linter } from "eslint";
  */
 
 // --------------------------------------------------------------------------------
 // Export
 // --------------------------------------------------------------------------------
 
-/** @type {import("eslint").Linter.RulesRecord} */
+/** @type {Linter.RulesRecord} */
 export default {
   /**
    * Enforce consistency of spacing after the start of a comment `//` or `/*`.
-   *
-   * @link @stylistic/js: {@link https://eslint.style/rules/js/spaced-comment}
-   * @link airbnb-base: {@link https://github.com/airbnb/javascript/blob/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules/style.js#L499-L509}
+   * @see https://eslint.style/rules/js/spaced-comment (@stylistic/js)
+   * @see https://github.com/airbnb/javascript/blob/eslint-config-airbnb-v19.0.4/packages/eslint-config-airbnb-base/rules/style.js#L499-L509 (airbnb-base)
    */
   '@stylistic/js/spaced-comment': [
     'error',
