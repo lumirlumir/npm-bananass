976a54d2ada35d2eeb2db467d4ee3f3c08eb36cc
루밀LuMir <rpfos@naver.com>
2026-02-21 22:43:00 +0900

    test(*): replace shell script with Node.js test files to make example tests twice as fast

---
+++ examples/solutions-bananass-cjs/solutions.test.mjs
@@ -0,0 +1,19 @@
+/*
+ * This test is used to test the commands of the project.
+ * It is executed by the `npm run test` command in `examples-solutions-bananass-*` directories.
+ */
+
+import { readdirSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { build, run } from 'bananass/commands';
+
+const cwd = import.meta.dirname;
+const outDir = resolve(cwd, 'bananass');
+const solutions = readdirSync(outDir).map(file => file.replace('.js', ''));
+
+// Check that the solutions can be built with both template types.
+await build(solutions, { cwd, build: { templateType: 'fs' } });
+await build(solutions, { cwd, build: { templateType: 'rl' } });
+
+// Check that the solutions can be run.
+await run(solutions, { cwd });
