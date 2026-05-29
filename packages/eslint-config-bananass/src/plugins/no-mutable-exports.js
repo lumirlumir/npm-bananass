74845dee188f4bac6fc711b82bc1a03863bb92f4
루밀LuMir <rpfos@naver.com>
2026-05-29 20:22:04 +0900

    wip

---
+++ packages/eslint-config-bananass/src/plugins/no-mutable-exports.js
@@ -0,0 +1,68 @@
+// @ts-nocheck -- TODO: Code was simply copied and pasted. Type annotations will be modified later.
+
+function getScope(context, node) {
+  const { sourceCode } = context;
+  if (sourceCode && sourceCode.getScope) {
+    return sourceCode.getScope(node);
+  }
+  return context.getScope();
+}
+
+/** @type {import('eslint').Rule.RuleModule} */
+export default {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      category: 'Helpful warnings',
+      description: 'Forbid the use of mutable exports with `var` or `let`.',
+      url: 'https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-mutable-exports.md',
+    },
+    schema: [],
+  },
+
+  create(context) {
+    function checkDeclaration(node) {
+      const { kind } = node;
+      if (kind === 'var' || kind === 'let') {
+        context.report(node, `Exporting mutable '${kind}' binding, use 'const' instead.`);
+      }
+    }
+
+    /** @type {(scope: import('eslint').Scope.Scope, name: string) => void} */
+    function checkDeclarationsInScope({ variables }, name) {
+      variables
+        .filter(variable => variable.name === name)
+        .forEach(variable => {
+          variable.defs
+            .filter(def => def.type === 'Variable' && def.parent)
+            .forEach(def => {
+              checkDeclaration(def.parent);
+            });
+        });
+    }
+
+    return {
+      /** @param {import('estree').ExportDefaultDeclaration} node */
+      ExportDefaultDeclaration(node) {
+        const scope = getScope(context, node);
+
+        if ('name' in node.declaration && node.declaration.name) {
+          checkDeclarationsInScope(scope, node.declaration.name);
+        }
+      },
+
+      /** @param {import('estree').ExportNamedDeclaration} node */
+      ExportNamedDeclaration(node) {
+        const scope = getScope(context, node);
+
+        if ('declaration' in node && node.declaration) {
+          checkDeclaration(node.declaration);
+        } else if (!('source' in node) || !node.source) {
+          node.specifiers.forEach(specifier => {
+            checkDeclarationsInScope(scope, specifier.local.name);
+          });
+        }
+      },
+    };
+  },
+};
