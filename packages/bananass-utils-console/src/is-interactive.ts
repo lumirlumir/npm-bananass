4328b5a80cfd03b30dc9a161d563ca05da640596
lumir <rpfos@naver.com>
2026-08-05 20:26:53 +0900

    wip

--- packages/bananass-utils-console/src/is-interactive.ts
+++ packages/bananass-utils-console/src/is-interactive.ts
@@ -5,6 +5,12 @@
  */
 
 // --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import type { WriteStream } from 'node:tty';
+
+// --------------------------------------------------------------------------------
 // Export
 // --------------------------------------------------------------------------------
 
@@ -14,16 +20,16 @@
  * It checks that the stream is [TTY](https://jameshfisher.com/2017/12/09/what-is-a-tty/), not a dumb terminal, and not running in a CI.
  *
  * This can be useful to decide whether to present interactive UI or animations in the terminal.
- * @param {NodeJS.WriteStream} [stream] The stream to check. (default: `process.stdout`)
- * @returns {boolean}
+ * @param stream The stream to check. (default: `process.stdout`)
+ * @returns `true` if the stream is interactive, `false` otherwise.
  * @example
- * ```
+ * ```js
  * import isInteractive from 'bananass-utils-console/is-interactive';
  *
  * isInteractive(); // true
  * ```
  */
-export default function isInteractive(stream = process.stdout) {
+export default function isInteractive(stream: WriteStream = process.stdout): boolean {
   return Boolean(
     stream && stream.isTTY && process.env.TERM !== 'dumb' && !('CI' in process.env),
   );
